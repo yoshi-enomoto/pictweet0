479e17f0ed0bbc3d65707fb319df5caa289e7356
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-13 11:41:47 +0900

    ツイート一覧を表示

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,13 @@
 class TweetsController < ApplicationController
+
+  def index
+    # 下記だけではレコード内の各カラムを表示するという都合の良い取り出しは出来ない為、
+    # 更にview側で『.カラム名』とする必要がある。
+    @tweet = Tweet.find(1)
+
+    # 同様に下記で取り出すには、view側でeachで回す
+    @tweets = Tweet.all
+
+  end
+
 end
