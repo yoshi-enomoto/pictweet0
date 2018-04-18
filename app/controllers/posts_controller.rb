2484c50692e52cb5f61c2d9293b748727958999e
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-18 20:06:51 +0900

    書き込み作成後のリダイレクト先の設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,20 @@ class PostsController < ApplicationController
   def create
     @post = Post.create(text: post_params[:text], detail_id: post_params[:detail_id], user_id: current_user.id)
 
-    # redirect_to "/users/#{@post.user_id}/details/#{@post.detail_id}"
-    redirect_to controller: 'details', action: 'show', id: 8
+    @user = User.new(id: user_params[:user_id])
 
-    # redirect_to "/users/:user_id/details/#{@post.detail_id}"
-    # binding.pry
-    # どっちが取得、この『.user_id』だと？
-    # →『@post』を作成したユーザーのid。
+    # redirect_to "/users/#{@post.user_id}/details/#{@post.detail_id}"
+    # この『.user_id』は『@post』を作成したユーザーのidが取れてしまう為、
+    # 『@user』を作成して、そのidを取得して、URLに渡してやる。
+    redirect_to "/users/#{@user.id}/details/#{@post.detail_id}"
   end
 
   private
   def post_params
     params.permit(:text, :detail_id)
+  end
+
+  def user_params
+    params.permit(:user_id)
   end
 end
