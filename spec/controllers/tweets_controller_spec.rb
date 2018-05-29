126a3660280b0726a547e4272904126bd0e5cb7c
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-29 20:10:22 +0900

    newアクションのテスト

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 describe TweetsController do
+  describe 'GET #new' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+
+  # describe 'HTTPメソッド名 #アクション名' do
+  #   it "インスタンス変数は期待した値になるか？" do
+  #   "擬似的にリクエストを行ったことにするコードを書く"
+  #     "エクスペクテーションを書く"
+  #   end
+
+  #   it "期待するビューに遷移するか？" do
+  #     "擬似的にリクエストを行ったことにするコードを書く"
+  #     "エクスペクテーションを書く"
+  #   end
+  # end
 
 end
