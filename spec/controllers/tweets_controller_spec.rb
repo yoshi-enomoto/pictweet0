b75432d25629dcd99b061c5f31aa056d37a917e2
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-29 20:30:37 +0900

    editアクションのテスト
    ※Factory_Girlによるダミーインスタンスも合わせて定義

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -8,6 +8,20 @@ describe TweetsController do
     end
   end
 
+  describe 'GET #edit' do
+    it "assigns the requested tweet to @tweet" do
+      tweet = create(:tweet)  #factory_girlでのダミーインスタンスの保存
+      get :edit, id: tweet    #これで、上の行で保存したインスタンスのidがセット出来る。
+      expect(assigns(:tweet)).to eq tweet
+    end
+
+    it "renders the :edit template" do
+      tweet = create(:tweet)
+      get :edit, id: tweet
+      expect(response).to render_template :edit
+    end
+  end
+
 
   # describe 'HTTPメソッド名 #アクション名' do
   #   it "インスタンス変数は期待した値になるか？" do
---
+++ spec/factories/tweets.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :tweet do
+    text         "hello!"
+    image        "hoge.png"
+    user_id      "1"
+  end
+end
+
+
