92b728caf488894708b9841627b702e75b8b60b4
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-29 20:52:08 +0900

    『faker』によるダミーデータ作成追記

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     text         "hello!"
     image        "hoge.png"
     user_id      "1"
+    created_at   { Faker::Time.between(2.days.ago, Time.now, :all) }
   end
 end
 
