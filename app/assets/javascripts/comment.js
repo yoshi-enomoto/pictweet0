0ee36c4280f843d8c21aa29c3601c445e255b251
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-04 18:47:47 +0900

    ３：非同期通信の設定

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,6 +2,20 @@ $(function() {
   $("#new_comment").on("submit", function(e) {
     e.preventDefault();
     //コメントフォームの情報を取得する為に使用。
+    // これを用いることで、『ajax』通信内の『data』にそのまま渡せる。
     var formData = new FormData(this);
+    // イベント内の『action』属性の値を取得する。そこが送り先のurlとなる為。
+    var url = $(this).attr("action");
+    $.ajax({
+      type: "post",
+      url : url,
+      data: formData,
+      dataType: "json"
+      // 双方共に『false』で上書きを防ぐ。
+      //クエリ文字列に変換する役割
+      processData: false,
+      //データのファイル形式を伝えるヘッダ
+      contentType: false
+    })
   });
 });
