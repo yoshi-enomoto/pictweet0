0645370141c6bc206f57469e053c56fb959594cb
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-04 19:20:24 +0900

    ６：json形式の保存成功時の処理

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,15 @@
 $(function() {
+
+  function buildHTML(comment) {
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${comment.user_id}>${comment.user_name}</a>
+                    ：${comment.text}
+                  </strong>
+                </p>`
+    return html;
+  }
+
   $("#new_comment").on("submit", function(e) {
     e.preventDefault();
     //コメントフォームの情報を取得する為に使用。
@@ -16,6 +27,14 @@ $(function() {
       processData: false,
       //データのファイル形式を伝えるヘッダ
       contentType: false
+    })
+    // 非同期通信成功時
+    .done(function(data){
+      // ビューにhtmlを差し込む。
+      var html = buildHTML(data);
+      $(".comments").append(html);
+      // 入力フォームに空の値を入れる。
+      $(".textbox").val("");
     })
   });
 });
