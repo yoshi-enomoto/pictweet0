2eb44f97023328e9f109a53416ceaf83a1dc31ce
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-11 16:31:22 +0900

    Ajax用の必要情報取得

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,5 +1,8 @@
 $(document).on("turbolinks:load", function() {
 
   $("#communication_area").on("submit", function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");
   });
 });
