ff4aa1701061b88d3f13f8b930751a9e1606d387
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-11 16:31:39 +0900

    Ajaxの送信内容の設定

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -4,5 +4,13 @@ $(document).on("turbolinks:load", function() {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr("action");
+    $.ajax({
+      type: "post",
+      url: url,
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
   });
 });
